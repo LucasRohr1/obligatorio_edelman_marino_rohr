3d925fc5e61e202789386a8a32f411f2af0e8344
martinedelman <martin@edelman.com.uy>
2022-06-21 17:26:40 -0300

    Revert "Perfil e inicio sesion comentado"
    This reverts commit 5c6af5a9097190463ee377c6e063afe48e62e26d.

--- src/interfaz/scripts/index.js
+++ src/interfaz/scripts/index.js
@@ -8,6 +8,7 @@ import ListaPeliculas from '../../dominio/lista-peliculas.mjs';
 import Pelicula from '../../dominio/pelicula.mjs';
 import ListaUsaurios from '../../dominio/pelicula.mjs'
 
+<<<<<<< HEAD
 import * as $ from "jquery/dist/jquery.min"
 
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
@@ -72,3 +73,43 @@ $(document).ready(function(){
 //     console.log(peliculas);
 //   }
 // })
+=======
+const listaPeliculas = new ListaPeliculas();
+
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+
+const tabBar = new MDCTabBar(document.querySelector(".mdc-tab-bar"));
+tabBar.listen("MDCTabBar:activated", (activatedEvent) => {
+  document.querySelectorAll(".content").forEach((element, index) => {
+    if (index === activatedEvent.detail.index) {
+      element.classList.remove("sample-content--hidden");
+    } else {
+      element.classList.add("sample-content--hidden");
+    }
+  });
+});
+
+const textFieldTitle = new MDCTextField(document.getElementById('title'));
+const textFieldYear = new MDCTextField(document.getElementById('year'));
+const selectGenre = new MDCSelect(document.querySelector('.mdc-select'));
+
+const addButton = new MDCRipple(document.getElementById('addButton'));
+addButton.listen('click', () => {
+  let title = textFieldTitle.value;
+  let year = textFieldYear.value;
+  let genre = selectGenre.value;
+  try {
+    let newPelicula = new Pelicula(title, genre, year);
+    listaPeliculas.agregar(newPelicula);
+  } catch (error) {
+    const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
+    snackbar.labelText = error.message;
+    snackbar.open();
+  } finally {
+    let peliculas = listaPeliculas.getPeliculas();
+    console.log(peliculas);
+  }
+})
+>>>>>>> parent of 5c6af5a (Perfil e inicio sesion comentado)
